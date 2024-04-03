1dd5e4272b94cc34748f474e5c4c6c8664df70c5
azizdeniz890 <153359251+azizdeniz890@users.noreply.github.com>
2024-04-04 00:12:04 +0300

    version2
    version2

--- hmpy.py
+++ hmpy.py
@@ -1,27 +1,74 @@
-while True:
-    print("İslem")
-    print("1. +")
-    print("2. -")
-    print("3. *")
-    print("4. /")
-    print("5. exit")
-    s = input("Seciminizi girin (1/2/3/4/5): ")
-    if s in ('1', '2', '3', '4'):
-        sayi1 = float(input("Birinci sayıyı gir: "))    
-        sayi2 = float(input("İkinci sayıyı gir: "))
-        if s == '1':
-            print("sonuc:", sayi1 + sayi2)
-        elif s == '2':
-            print("sonuc:", sayi1 - sayi2)
-        elif s == '3':
-            print("sonuc:", sayi1 * sayi2)
-        elif s == '4':
-            if sayi2 == 0:
-                print("sıfıra bolunmuyor")
+#update- yuzde alma
+class HesapMakinesi:
+    def __init__(self):
+        pass
+    
+    def menu_goster(self):
+        print("lutfen islem seciniz")
+        print("1. toplama")
+        print("2. çıkarma")
+        print("3. çarpma")
+        print("4. bölme")
+        print("5. yüzde")
+        print("6. çıkış")
+    
+    def toplama(self, s1, s2):
+        
+        return s1 + s2
+    
+    def cikarma(self, s1, s2):
+        return s1 - s2
+    
+    def carpma(self, s1, s2):
+        return s1 * s2
+    
+    def bolme(self, s1, s2):
+        if s2 == 0:
+            return "sıfıra bölünemez"
+        else:
+            
+            return s1 / s2
+    
+    
+    def yuzde(self, s1, yuzde):
+        return s1 * (yuzde / 100)
+    
+    
+    
+    def hesapla(self):
+        while True:
+            self.menu_goster()
+            
+            secim = input("seçiminizi girin: ")
+            
+            if secim == '6':
+                print("çıkış yapi")
+                break
+            
+            if secim in ('1', '2', '3', '4', '5'):
+                s1 = float(input("birinci sayıyı girin: "))
+                #sıkıntı 
+                if secim != '5':
+                    s2 = float(input("ikinci sayıyı girin: "))
+                
+                if secim == '1':
+                    
+                    print("sonuc:", self.toplama(s1, s2))
+                elif secim == '2':
+                    
+                    print("sonuc:", self.cikarma(s1, s2))
+                elif secim == '3':
+                    
+                    print("sonuc:", self.carpma(s1, s2))
+                elif secim == '4':
+                    
+                    print("sonuc:", self.bolme(s1, s2))
+                elif secim == '5':
+                    
+                    yuzde_sayi = float(input("yüzdeyi girin: "))
+                    print("sonuc:", self.yuzde(s1, yuzde_sayi))
             else:
-                print("Sonuc:", sayi1 / sayi2)
-    elif s == '5':
-        print("cıkıs yapılıyor")
-        break
-    else:
-        print("lutfen aralıkta bir rakam secin")
+                print("Hata:lutfen 1,23,4,5 içinden bir sayi girin.______hata_____")
+
+hm = HesapMakinesi()
+hm.hesapla()
